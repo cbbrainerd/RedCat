a54bc53ccc3eb1783fe22d1ad0a7061ee398cc91
cbbrainerd <cbbrainerd@ucdavis.edu>
2015-02-05 15:53:22 -0800

    Starting to write new code for all the serial stuff. Very minimal at the moment.

---
+++ Runs.c
@@ -0,0 +1,56 @@
+class Run;
+
+class CreateRun {
+public:
+    Run();
+    Run& runsPerPowerCycle(unsigned int runs);
+    Run& numberPowerCycles(unsigned int pc);
+    Run& data(unsigned short databyte);
+    Run& tryReread(unsigned int reread);
+    Run& inputMode(unsigned short mode);
+    Run& saveTo(std::string filename);
+private:
+    friend class Run;
+    unsigned int numRuns_;
+    unsigned int numPC_;
+    unsigned short data_;
+    unsigned int numTries_;
+    unsigned int whichMode_;
+    std::string logFile_;
+};
+
+inline CreateRun::CreateRun()
+    : numRuns_      (0)
+    , numPC_        (0)
+    , dataByte_     (170)
+    , numTries_     (100)
+    , whichMode_    (3)
+    , logFile_      ("logFile")
+{ }
+
+inline CreateRun& CreateRun::runsPerPowerCycle(unsigned int runs)
+{ numRuns_ = runs; return *this; }
+
+inline CreateRun& CreateRun::numberPowerCycles(unsigned int pc)
+{ numPC_ = pc; return *this; }
+
+inline CreateRun& CreateRun::data(unsigned short dat)
+{ dataByte_ = dat; return *this; }
+
+inline CreateRun& inputMode(unsigned short mode)
+{ whichMode_ = mode; return *this; }
+
+inline CreateRun& tryReread(unsigned int reread)
+{ numTries_ = reread; return *this }
+
+inline CreateRun& saveTo(std::string filename)
+{ logFile_ = filename; return *this }
+
+class Run {
+Public: 
+    Run(CreateRun const& params);
+}
+
+Run::Run(CreateRun const& params) {
+
+}
